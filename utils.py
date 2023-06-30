728e16a8d8328b54f8f1d13083a77b79f04f55ab
Dongeon Kim <60685175+ceo21ckim@users.noreply.github.com>
2023-06-30 09:48:22 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import torch 
+import torchgeometry
 import numpy as np 
 
+from sklearn.metrics import roc_curve, auc 
+
+
 import matplotlib.pyplot as plt 
+import matplotlib as mpl
+import matplotlib.colors as mcl
+import matplotlib.cm as cm
 
 def torch2npy(tensor):
     if len(tensor.shape) == 4:
@@ -21,4 +28,59 @@ def figure(img, reshape=(1, 2, 0), savefig=False, fname=None):
     
     if savefig:
         plt.savefig(f'{fname}.pdf', dpi=100)
+    plt.show()
+
+
+def set_seed(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def roc_measure(target, preds):
+    fpr, tpr, _ = roc_curve(target, preds, pos_label=1)
+    auroc = metrics.auc(fpr, tpr)
+    return auroc
+
+
+def scaler(x):
+    if isinstance(x, torch.Tensor):
+        _min = torch.min(x)
+        _max = torch.max(x)
+        
+    elif isinstance(x, np.array):
+        _min = np.min(x)
+        _max = np.max(x)
+
+    outs = (x - _min) / (_max - _min)
+    return outs
+
+    
+def loss_ssim(preds, target):
+    ssim = torchgeometry.losses.SSIM(11, reduction='mean')
+    x_ssim = ssim(preds, target)
+    
+    return x_ssim
+
+
+
+def segmap_figure(target, preds, norm=True, t=(1, 2, 0), save=None, path=None, alpha=0.5):
+    target = torch2npy(target, norm, t)
+    preds = torch2npy(preds, norm, t)
+    
+    cmap = cm.hot
+    vmax = preds.max()
+    vmin = preds.min()
+    norms = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+    
+    colormapping = cm.ScalarMappable(norm=norms, cmap=cmap)
+    
+    plt.colorbar(colormapping, ax=plt.gca())
+    plt.imshow(target)
+    plt.imshow(preds, cmap=cmap, alpha=alpha)
+    
+    if save:
+        plt.savefig(path, dpi=200)
     plt.show()
